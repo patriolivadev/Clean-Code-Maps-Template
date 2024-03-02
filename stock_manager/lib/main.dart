bca8768002c4fd7dacaef040d6107172cbb4b6df
Patricio Oliva <patrioliva2015@gmail.com>
2024-03-02 20:32:35 -0300

    Initial configurations: - HomePage - goRouter - Bloc, getIt

--- stock_manager/lib/main.dart
+++ stock_manager/lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:stock_manager/core/routes/router.dart';
+import 'package:stock_manager/core/services/dependencies_service.dart';
 
-void main() {
+void main() async {
+  await configureDependencies(Environment.dev);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
-      home: Container(),
+      routerConfig: router,
     );
   }
 }
