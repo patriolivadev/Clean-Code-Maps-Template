a40a875ed2ea31cd0ea458e34d808854d880af65
Patricio Oliva <patrioliva2015@gmail.com>
2024-07-09 15:10:39 -0300

    Se quitó firebase por completo. FireStore era el problema que generaba más de no sé cúántos métodos y me solicitaba multidex.

--- stock_manager/lib/main.dart
+++ stock_manager/lib/main.dart
@@ -1,17 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:injectable/injectable.dart';
 import 'package:stock_manager/core/routes/router.dart';
 import 'package:stock_manager/core/services/dependencies_service.dart';
 import 'package:stock_manager/core/utils/themes.dart';
-import 'package:stock_manager/core/services/firebase_options.dart';
 
 void main() async {
   await configureDependencies(Environment.dev);
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);  runApp(const MyApp());
