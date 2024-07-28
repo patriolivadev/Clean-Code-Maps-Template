b91cf2aefeffe0220a0fb34f2854ad94d340fb35
Patricio Oliva <patrioliva2015@gmail.com>
2024-07-28 19:15:03 -0300

    Se renombraron archivos.

--- stock_manager/lib/features/product/data/models/product_model.dart
+++ stock_manager/lib/features/product/data/models/product_model.dart
@@ -1,4 +1,4 @@
-import 'package:stock_manager/features/products/domain/entities/product.dart';
+import 'package:stock_manager/features/product/domain/entities/product.dart';
 
 class ProductModel extends Product {
   ProductModel({
